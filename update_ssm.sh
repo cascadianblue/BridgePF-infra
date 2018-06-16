8f517ce1eeea387dff6f45c449f72e0d6c14e868
Khai Do <zaro0508@gmail.com>
2018-06-16 10:39:59 -0700

    fix securestring values in parameter store

--- update_ssm.sh
+++ update_ssm.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+# double interpolate vars from travis
+eval export "BridgeHealthcodeRedisKey=\$BridgeHealthcodeRedisKey_$TRAVIS_BRANCH"
+eval export "EmailUnsubscribeToken=\$EmailUnsubscribeToken_$TRAVIS_BRANCH"
+eval export "HibernateConnectionPassword_=\$HibernateConnectionPassword_y_$TRAVIS_BRANCH"
+eval export "HibernateConnectionUrl=\$HibernateConnectionUrl_$TRAVIS_BRANCH"
+eval export "HibernateConnectionUsername=\$HibernateConnectionUsername_$TRAVIS_BRANCH"
+eval export "SynapseApiKey=\$SynapseApiKeyy_$TRAVIS_BRANCH"
+eval export "SynapseUser=\$SynapseUser_$TRAVIS_BRANCH"
+
 # CF does not support SecureString, update with AWS CLI instead.
 # !!Important!! - this only adds, it does not cleanup.  Make sure to cleanup using the AWS console or CLI (delete-parameter command)
 KMS_KEY_ID="alias/bridgepf-$TRAVIS_BRANCH/KmsKey"
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/BRIDGE_HEALTHCODE_REDIS_KEY" --value "$BridgeHealthcodeRedisKey_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/EMAIL_UNSUBSCRIBE_TOKEN" --value "$EmailUnsubscribeToken_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/HIBERNATE_CONNECTION_PASSWORD" --value "$HibernateConnectionPassword_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/HIBERNATE_CONNECTION_URL" --value "$HibernateConnectionUrl_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/HIBERNATE_CONNECTION_USERNAME" --value "$HibernateConnectionUsername_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/SYNAPSE_API_KEY" --value "$SynapseApiKey_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
-aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/SYNAPSE_USER" --value "$SynapseUser_$TRAVIS_BRANCH" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/BRIDGE_HEALTHCODE_REDIS_KEY" --value "$BridgeHealthcodeRedisKey" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/EMAIL_UNSUBSCRIBE_TOKEN" --value "$EmailUnsubscribeToken" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/HIBERNATE_CONNECTION_PASSWORD" --value "$HibernateConnectionPassword" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/HIBERNATE_CONNECTION_URL" --value "$HibernateConnectionUrl" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/HIBERNATE_CONNECTION_USERNAME" --value "$HibernateConnectionUsername" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/SYNAPSE_API_KEY" --value "$SynapseApiKey" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
+aws ssm put-parameter --name "/bridgepf-$TRAVIS_BRANCH/SYNAPSE_USER" --value "$SynapseUser" --type "SecureString" --key-id "$KMS_KEY_ID" --overwrite
